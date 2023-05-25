c6b7c187475ed9c9c8873d626e56d13e0362da4b
frcs <francois.pitie@gmail.com>
2023-05-25 22:37:55 +0100

    we're v0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bbquiz',
-      version='0.1',
+      version='0.2',
       author='Francois Pitie',
       packages=find_packages(),
       # packages=['bbquiz',
